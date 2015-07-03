bce8e9a4d4515ad0bb908365338e6d0cbfc2d9bc
jdoper <atomas395@gmail.com>
2015-07-03 15:39:52 -0300

    Finalizando metodos de VeiculoResource para responder as chamadas externas

---
+++ src/com/infoplacas/model/RequestResponse.java
@@ -0,0 +1,32 @@
+package com.infoplacas.model;
+
+public class RequestResponse {
+	private boolean success;
+	private String mensagem;
+	
+	public RequestResponse() {
+		this.success = true;
+	}
+	
+	public RequestResponse(String mensagem) {
+		super();
+		this.success = false;
+		this.mensagem = mensagem;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+	
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+}
--- src/com/infoplacas/resource/VeiculoResource.java
+++ src/com/infoplacas/resource/VeiculoResource.java
@@ -12,10 +12,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.StatusType;
 
 import com.infoplacas.dao.VeiculoDAO;
+import com.infoplacas.model.RequestResponse;
 import com.infoplacas.model.Veiculo;
 
 @Stateless
@@ -24,34 +27,57 @@ public class VeiculoResource {
 	@EJB
 	private VeiculoDAO veiculoDAO;
 	
+	/*
+	 * GetAll
+	 * */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Veiculo> listarVeiculos() {
 		return veiculoDAO.getAllVeiculos();
 	}
 	
+	/*
+	 * Buscar Veiculo
+	 * */
 	@POST
-	@Consumes("application/x-www-form-urlencoded")
+	@Path("/buscar")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response pesquisarVeiculo(@FormParam("placa") String placa) {
+	public Response pesquisarVeiculo(@QueryParam("placa") String placa) {
 		Veiculo veiculo = veiculoDAO.getVeiculo(placa);
-		return Response.ok(veiculo).build();
+		return Response.status(200).entity(veiculo).build();
 	}
 	
 	/*
-	 * Criar Veiculo
+	 * Cadastrar Veiculo
 	 * */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response criarVeiculo(Veiculo veiculo) {
+		veiculoDAO.salvar(veiculo);
+		return Response.status(201).entity(new RequestResponse()).build();
+	}
 	
 	/*
 	 * Editar Veiculo
 	 * */
+	@POST
+	@Path("/editar")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response editarVeiculo(Veiculo veiculo) {
+		veiculoDAO.atualizar(veiculo);
+		return Response.status(200).entity(new RequestResponse()).build();
+	}
 	
+	/*
+	 * Excluir Veiculo
+	 * */
 	@POST
 	@Path("/excluir")
-	@Consumes("application/x-www-form-urlencoded")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response excluirVeiculo(@FormParam("placa") String placa) {
+	public Response excluirVeiculo(@QueryParam("placa") String placa) {
 		veiculoDAO.remover(placa);
-		return Response.ok(placa).build();
+		return Response.status(200).entity(new RequestResponse()).build();
 	}
 }
