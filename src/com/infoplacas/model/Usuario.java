d83dd721d81d9d050bacbbf4fa03d568cc0d0202
jdoper <atomas395@gmail.com>
2015-07-20 10:27:47 -0300

    Excluindo imports desnecessários

--- src/com/infoplacas/model/Usuario.java
+++ src/com/infoplacas/model/Usuario.java
@@ -1,14 +1,11 @@
 package com.infoplacas.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
